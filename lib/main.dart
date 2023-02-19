25ee18c996a5b192e2a14f19a501f8103c946162
JohanAnquetil <anquetil.johan@laposte.net>
2023-02-19 01:15:13 +0100

    page d'accueil bien avancée

--- lib/main.dart
+++ lib/main.dart
@@ -30,11 +30,18 @@ class MyApp extends StatelessWidget {
 class BasicsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var size = MediaQuery.of(context).size;
-    var platform = Theme.of(context).platform;
+    var size = MediaQuery
+        .of(context)
+        .size;
+    var platform = Theme
+        .of(context)
+        .platform;
     print("size: $size");
     print("platform: $platform");
-    double width = MediaQuery.of(context).size.width;
+    double width = MediaQuery
+        .of(context)
+        .size
+        .width;
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -46,29 +53,22 @@ class BasicsPage extends StatelessWidget {
         backgroundColor: Colors.white,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: SizedBox(
-          child: Image.asset('img/1200px-Libération.svg.png',
-              height: 100, width: 100),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Libé'),
+            Container(
+              //  margin: const EdgeInsets.only(right: 75),
+              child: Image.asset(
+                'img/1200px-Libération.svg.png',
+                width: 100,
+              ),
+            ),
+          ],
         ),
       ),
       body: SingleChildScrollView(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Row(
@@ -82,45 +82,255 @@ class BasicsPage extends StatelessWidget {
                 ),
               ],
             ),
-            Row(
-              children:[
-                Container(
-                width: size.width,
-                  child: Row(
-                  children:[
-                  redTitle("WANTED"),
-                    blackTitle("Deux développeurs repentis en cavale en recherche d'une alternance")
-            ],
-            ),
+            Padding(
+              padding: EdgeInsets.only(top: 10, left: 5),
+              child: Wrap(
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        redTitle("WANTED "),
+                        blackTitle(
+                            "DEUX DEVELOPPEURS EN RECHERCHE D'ALTERNANCE PRETS A REVOLUTIONNER L'APPLI DE LIBERATION"),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                      padding: EdgeInsets.only(
+                        top: 7,
+                        left: 4,
+                      ),
+                      child: paragraph(
+                          "Fraichement issus du centre de formation d'Ada Tech School dans le 10e arrondissement de Paris (si t'es pas le numéro 10 à Paname...), Les plus si jeunes développeurs en puissance (75 ans à eux deux), seront de précieux alliés pour les projets tech de Libération. Plus à l'aise à domicile sur leurs terrains favoris (Dart, JavaScript, C#), ils sont complètement adaptables à d'autres contraintes techniques (Swift, Kotlin, Java, PHP...). Ils ont le bénéfice de leur grand âge, cette expérience qui permet d'être calme et productif même dans des contextes stressants et d'être les moteurs d'une dynamique collective."),),
+                ],
               ),
+            ),
+            Row(
+              children: [
+                Padding(padding: EdgeInsets.only(top: 20)),
+                theme("Chômage"),
+                time("Il y a 5 minutes"),
+                abonne(),
+                Expanded(
+                  flex: 3,
+                  child: Padding(
+                    padding: EdgeInsets.only(right: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [Icon(Icons.bookmark_border)],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Divider(
+              thickness: 2,
+            ),
+            Padding(padding: EdgeInsets.only(top: 10, left: 10),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    flex:2,
+                    child: Wrap(
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            children: [
+                              redTitle2("NASTIA LA ROUGE "),
+                              blackTitle2("FROM MARIES WITH RABIES"),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                            padding: EdgeInsets.only(
+                              top: 7,
+                              left: 2,
+                            ),
+                            child: paragraph("Née en Union Soviétique qu'elle connaîtra près d'une décennie avant sa chute, elle a passé une adolescence agitée dans la toute neuve Russie. Elle a acquis une conscience politique très jeune grâce aux traductions du journal Libération opérées par le S.F.I.O (Syndicat Fédéral d'Iochkar-Ola) envoyés par des habitants de sa fantastique ville jumelle Française, Bourges. Délivrant ses messages philosophico-politiques grâce à l'art du Tag sur les murs de l'agglomération, elle a réalisé ses premiers sabotages de navires à l'âge de 6 ans sur les bords du Malaya Kokshaga. Soucieuse de défendre un monde plus juste et la liberté de la presse, elle apprend actuellement les rudiments du développement entre deux manifestations Parisiennes.")
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(child: Padding(padding: EdgeInsets.only(left:5),
+                    child: Container(
+                      child: Image.asset(
+                        'img/NASTIA.JPG',
+                      ),
+                    ),
+                  )
+                  )
+                ],
+              ),
+            ),
+            Row(
+              children: [
+                Padding(padding: EdgeInsets.only(top: 20)),
+                theme("Candidature"),
+                time("Il y a 25 minutes"),
+                abonne(),
+                Expanded(
+                  flex: 3,
+                  child: Padding(
+                    padding: EdgeInsets.only(right: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [Icon(Icons.bookmark_border)],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Divider(
+              thickness: 2,
+            ),
+            Padding(padding: EdgeInsets.only(top: 10, left: 10),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    flex:2,
+                    child: Wrap(
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            children: [
+                              redTitle2("JOJO LE BOX-TO-BOX "),
+                              blackTitle2("POLYVALENT ET DETERMINE QUELQUE SOIT SON POSTE SUR LA GAUCHE DU TERRAIN"),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                            padding: EdgeInsets.only(
+                              top: 7,
+                              left: 2,
+                            ),
+                            child: paragraph("Aussi à l'aise à la récupération grâce à sa fougue, à la relance avec son énergie ou en meneur de jeu avec sa créativité et sa vision stratégique, son expérience de la gestion de projet, de la mobilisation citoyenne et du développement du pouvoir d'agir abattront les défis techniques. Comme le prouve la photo, il sait être drôle parfois. Et aussi fièrement abonné à la meilleure Newsletter de la toile Chez Pol.")
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(child: Padding(padding: EdgeInsets.only(left:5),
+                    child: Container(
+                      child: Image.asset(
+                        'img/JOHAN.JPG',
+                      ),
+                    ),
+                  )
+                  )
+                ],
+              ),
+            ),
+            Row(
+              children: [
+                Padding(padding: EdgeInsets.only(top: 20)),
+                theme("Révolution"),
+                time("Il y a 45 minutes"),
+                abonne(),
+                Expanded(
+                  flex: 3,
+                  child: Padding(
+                    padding: EdgeInsets.only(right: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [Icon(Icons.bookmark_border)],
+                    ),
+                  ),
+                ),
+              ],
+            ),
                 ],
             ),
-          ],
-        ),
       ),
     );
   }
 }
 
-Text redTitle(String title) {
-  return Text(
-    "$title",
-    style:
-        TextStyle(color: Colors.red, fontWeight: FontWeight.bold, fontSize: 20),
-  );
+TextSpan redTitle(String title) {
+  return TextSpan(
+      text: "$title",
+      style: TextStyle(
+          fontWeight: FontWeight.bold, color: Colors.red, fontSize: 20));
 }
 
-Text blackTitle(String title) {
-  return Text(
-    "$title",
-    style: TextStyle(
-        color: Colors.black, fontWeight: FontWeight.bold, fontSize: 20),
-  );
+TextSpan blackTitle(String title) {
+  return TextSpan(
+      text: "$title",
+      style: TextStyle(
+          fontWeight: FontWeight.bold, color: Colors.black, fontSize: 20));
 }
 
 Text paragraph(String paragraph) {
-  return Text(
-    "$paragraph",
-    style: TextStyle(color: Colors.black45, fontSize: 10),
+  return Text("$paragraph",
+      style: TextStyle(color: Colors.black87, fontSize: 10));
+}
+
+TextSpan redTitle2(String title) {
+  return TextSpan(
+      text: "$title",
+      style: TextStyle(
+          fontWeight: FontWeight.bold, color: Colors.red, fontSize: 15));
+}
+
+TextSpan blackTitle2(String title) {
+  return TextSpan(
+      text: "$title",
+      style: TextStyle(
+          fontWeight: FontWeight.bold, color: Colors.black, fontSize: 15));
+}
+
+Text paragraph2(String paragraph) {
+  return Text("$paragraph",
+      style: TextStyle(color: Colors.black87, fontSize: 8));
+}
+
+Expanded theme(String theme) {
+  return Expanded(
+    child: Padding(
+      padding: EdgeInsets.only(left: 10),
+      child: Container(
+        child: RichText(
+          text: TextSpan(
+            text: "$theme",
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black45,
+                fontSize: 9),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+Expanded time(String time) {
+  return Expanded(
+    child: Padding(
+      padding: EdgeInsets.only(left: 0),
+      child: Container(
+        child: RichText(
+          text: TextSpan(
+            text: "$time",
+            style: TextStyle(color: Colors.black45, fontSize: 9),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+Expanded abonne() {
+  return Expanded(
+    child: Padding(
+      padding: EdgeInsets.only(left: 0),
+      child: Container(
+        child: RichText(
+          text: TextSpan(
+            text: "Abonné",
+            style: TextStyle(color: Colors.amber, fontSize: 9),
+          ),
+        ),
+      ),
+    ),
   );
 }
