c4d8ccf8bbf7ea39a2499d94e900683a7deb22b4
JohanAnquetil <anquetil.johan@laposte.net>
2023-03-05 19:29:32 +0100

    ??

--- lib/views/homePage.dart
+++ lib/views/homePage.dart
@@ -25,7 +25,7 @@ class homePage extends StatelessWidget {
             paragraphMainArticle: "Pensionnaires du centre de formation de l'équipe féministe Ada Tech School dans le 10e arrondissement de Paris (si t'es pas le numéro 10 à Paname...), les plus si jeunes développeurs en puissance, 75 ans à eux deux quand même, seront de précieux alliés pour les projets tech de Libération.",
             themeMainArticle: "Chômage",
             writtenBy: "Anastasia et Johan",
-            publishDateParam: "2023-03-04 10:48:00",
+            publishDateParam: "2023-03-04 12:40:00",
             legendPicture: "En recherche d'alternance pour la période de mi-juillet 2023 à mi-juin 2024 sur un rythme de 4 jours sur 5 en entreprise",
             completeArticle: "Ils ont le bénéfice de leur grand âge, en capacité à résister à la pression, apprécie le travail en équipe, empathiques et de grandes qualités relationnelles. Ils savent aussi se mettre au diapason de la dynamique collective et s'y fondre. Plus à l'aise à domicile sur leurs terrains de prédilection (Dart, JavaScript, C#), ils sont complètement adaptables à d'autres contraintes techniques (Swift, Kotlin, Java, PHP...).",
           ),
