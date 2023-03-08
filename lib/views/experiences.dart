7cbc5cb52cf202f49ad76e264843824e10e8baaa
Nastiakor <a.korotkova@yahoo.fr>
2023-03-08 15:33:41 +0100

    Expériences Anastasia

--- lib/views/experiences.dart
+++ lib/views/experiences.dart
@@ -11,40 +11,49 @@ class Experiences extends StatelessWidget {
     var width = size.width;
 
     return SingleChildScrollView(
-      child: Column(
-          children: [
-            MainArticle(
-              imagePath: "",
-              title: " ",
-              titlethen:
+      child: Column(children: [
+        MainArticle(
+          imagePath: "img/Experiences/Full-lobby-Hotel-Fabric.webp",
+          title: "2018 - 2022 RECEPTIONNISTE",
+          titlethen: " HOTELS A PARIS",
+          paragraphMainArticle:
+              "Accueil et renseignement de la clientèle, conciergerie, travail administratif, réponses aux commentaires des clients sur divers sites web, rédaction et traduction en anglais des brochures de communication",
+          themeMainArticle: "",
+          writtenBy: "Anastasia Korotkova",
+          publishDateParam: "2023-03-06 15:13:00",
+          legendPicture: "",
+          completeArticle:
+              "- 2021 - 2022 Hôtel Paradis\n- 2018 - 2020 Hôtel Fabric\n- 2017 - 2018 Hôtel des Marronniers\n",
+        ),
+        SecondaryArticle(
+          imagePath: "img/Experiences/ERGONCAPITALPARTNERS.png",
+          title:
+              "2010 - 2016 ASSISTANTE DE DIRECTION GÉNÉRALE / OFFICE MANAGER",
+          titlethen: " ",
+          paragraphMainArticle:
+              "Accueil physique et téléphonique, interprétariat, traduction de documents, gestion d'agenda, organisation de réunions et de déplacements, gestion des dépenses, organisation du bureau",
+          themeMainArticle: "",
+          writtenBy: "Anastasia Korotkova",
+          publishDateParam: "2023-03-06 10:13:00",
+          legendPicture: "",
+          completeArticle:
+              "- 2014 - 2016 Ergon Capital Advisors France\n- 2011 - 2014 Rietumu Bank\n- 2010 Moreno, Ruby et associés",
+        ),
+        SecondaryArticle(
+          imagePath: "img/Experiences/CiteDeLaMusique.jpg",
+          title:
+              "2011 HOTESSE D'ACCUEIL",
+          titlethen: " CITÉ DE LA MUSIQUE",
+          paragraphMainArticle:
+              "Accueil et renseignement des visiteurs, en français et en anglais. Préparation et organisation des évènements culturels. Traduction de brochures et de documents publicitaires (français - anglais)",
+          themeMainArticle: "",
+          writtenBy: "Anastasia Korotkova",
+          publishDateParam: "2023-03-06 10:13:00",
+          legendPicture: "",
+          completeArticle:
               "",
-              paragraphMainArticle:
-              "",
-              themeMainArticle: "",
-              writtenBy: "",
-              publishDateParam: "2023-03-06 10:13:00",
-              legendPicture:
-              "",
-              completeArticle:
-              "",
-            ),
-            SecondaryArticle(
-              imagePath: "",
-              title: "",
-              titlethen:
-              "",
-              paragraphMainArticle:
-              "",
-              themeMainArticle: "",
-              writtenBy: "",
-              publishDateParam: "2023-03-06 10:13:00",
-              legendPicture:
-              "",
-              completeArticle:
-              "",
-            ),
-          ]
-      ),
+        ),
+      ]),
     );
   }
 }
