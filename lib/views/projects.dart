020a6aec0cb43b0cb21b201bd0ceab8cc61d57d3
Nastiakor <a.korotkova@yahoo.fr>
2023-03-12 20:57:56 +0100

    :sparkles:

--- lib/views/projects.dart
+++ lib/views/projects.dart
@@ -11,40 +11,67 @@ class Projects extends StatelessWidget {
     var width = size.width;
 
     return SingleChildScrollView(
-      child: Column(
-          children: [
-            MainArticle(
-              imagePath: "",
-              title: " ",
-              titlethen:
-              "",
-              paragraphMainArticle:
-              "",
-              themeMainArticle: "",
-              writtenBy: "",
-              publishDateParam: "2023-03-06 10:13:00",
-              legendPicture:
-              "",
-              completeArticle:
-              "",
-            ),
-            SecondaryArticle(
-              imagePath: "",
-              title: "",
-              titlethen:
-              "",
-              paragraphMainArticle:
-              "",
-              themeMainArticle: "",
-              writtenBy: "",
-              publishDateParam: "2023-03-06 10:13:00",
-              legendPicture:
-              "",
-              completeArticle:
-              "",
-            ),
-          ]
-      ),
+      child: Column(children: [
+        MainArticle(
+          imagePath: "",
+          title: " ",
+          titlethen: "",
+          paragraphMainArticle: "",
+          themeMainArticle: "",
+          writtenBy: "",
+          publishDateParam: "2023-03-06 10:13:00",
+          legendPicture: "",
+          completeArticle: "",
+        ),
+        SecondaryArticle(
+          imagePath: "img/Projets/Pico8.png",
+          title: "JEU RETRO PICO 8",
+          titlethen: "",
+          paragraphMainArticle: "",
+          themeMainArticle: "",
+          writtenBy: "Anastasia Korotkova",
+          publishDateParam: "2023-03-08 16:10:00",
+          legendPicture: "",
+          completeArticle:
+              "- Durée: 2 semaines\n- Moteur: Pico-8\n- Langage : Lua\n\nNotre groupe, composé de 3 personnes, travaillait sur un jeu d'aventure où un pirate doit trouver 3 clés sur une île déserte afin de pouvoir ouvrir un coffre aux trésors.",
+        ),
+        SecondaryArticle(
+          imagePath: "img/Projets/Dataviz.png",
+          title: "DATAVIZ & API",
+          titlethen: "",
+          paragraphMainArticle: "",
+          themeMainArticle: "",
+          writtenBy: "Anastasia Korotkova",
+          publishDateParam: "2023-03-08 16:40:00",
+          legendPicture: "",
+          completeArticle:
+              "- Durée: 2 semaines\n- Langage : JavaScript\n\n.",
+        ),
+        SecondaryArticle(
+          imagePath: "img/Projets/My_mood.png",
+          title: "EXTENSION DE NAVIGATEUR",
+          titlethen: "",
+          paragraphMainArticle: "",
+          themeMainArticle: "",
+          writtenBy: "Anastasia Korotkova",
+          publishDateParam: "2023-03-08 16:40:00",
+          legendPicture: "",
+          completeArticle:
+          "- Durée: 2 semaines\n- Langage : JavaScript\n- Gestion de base de données: MongoDB\n.",
+        ),
+        SecondaryArticle(
+          imagePath: "",
+          title: "RESEAU SOCIAL PHP",
+          titlethen: "",
+          paragraphMainArticle: "",
+          themeMainArticle: "",
+          writtenBy: "Anastasia Korotkova",
+          publishDateParam: "2023-03-08 17:10:00",
+          legendPicture: "",
+          completeArticle:
+          "- Durée: 2 semaines\n- Langage : JavaScript\n- Gestion de base de données: MongoDB\n.",
+        ),
+      ]),
     );
   }
 }
