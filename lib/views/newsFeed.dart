b09517d68c8d7e9d8a417b755dc6c6ce58af0514
JohanAnquetil <anquetil.johan@laposte.net>
2023-03-21 09:24:36 +0100

    Fil info almost finish

--- lib/views/newsFeed.dart
+++ lib/views/newsFeed.dart
@@ -5,6 +5,12 @@ import 'package:http/http.dart' as http;
 import 'package:url_launcher/url_launcher.dart';
 import 'package:cv_flutter_libe/Controllers/bottomNavigationBar.dart';
 import 'package:cv_flutter_libe/Controllers/BottomBarArticle.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_web_browser/flutter_web_browser.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+
+const titleColor = Color(0XFFE60004);
 
 void main() => runApp(NYTAPI());
 
@@ -27,6 +33,12 @@ class _NYTAPIState extends State<NYTAPI> {
     if (response.statusCode == 200) {
       setState(() {
         _articles = json.decode(response.body)["results"];
+        // sort the articles by date of update
+        _articles.sort((a, b) {
+          DateTime aUpdated = DateTime.parse(a['updated']);
+          DateTime bUpdated = DateTime.parse(b['updated']);
+          return bUpdated.compareTo(aUpdated); // sort in descending order
+        });
       });
     } else {
       throw Exception('Failed to fetch articles');
@@ -42,93 +54,104 @@ class _NYTAPIState extends State<NYTAPI> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'NYT Most Popular',
-      home: Scaffold(
-        appBar: MyAppBarFeed(),
-        body: _articles.isNotEmpty
-            ? ListView.builder(
-          itemCount: _articles.length,
-          itemBuilder: (BuildContext context, int index) {
-            var updated =
-            _articles[index]['updated'].toString().substring(11, 16);
-            return InkWell(
-              onTap: () async {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) =>
-                        ArticlePage(article: _articles[index]),
-                  ),
-                );
-              },
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      width: 50,
-                      child: Column(
-                        children: [
-                          Text(
-                            updated,
-                            style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.bold,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                          SizedBox(height: 5),
-                          Container(
-                            height: 40,
-                            child: VerticalDivider(
-                              thickness: 2,
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(width: 10),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            _articles[index]['title'],
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18,
-                            ),
-                          ),
-                          SizedBox(height: 5),
-                          Text(
-                            _articles[index]['nytdsection'],
-                            style: TextStyle(
-                              color: Colors.grey[600],
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(height: 10),
-                          Divider(
-                            color: Colors.grey,
-                            thickness: 0.5,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            );
-          },
-        )
-            : Center(child: CircularProgressIndicator()),
-        bottomNavigationBar: MyBottomHomeNavigationBar(currentIndex: 1),
-      ),
+        title: 'NYT Most Popular',
+        home: Scaffold(
+            appBar: MyAppBarFeed(),
+            body: _articles.isNotEmpty
+                ? ListView.builder(
+                itemCount: _articles.length,
+                itemBuilder: (BuildContext context, int index) {
+            // get the updated date and time of the article
+            DateTime updatedDateTime = DateTime.parse(_articles[index]['updated']);
+        DateTime now = DateTime.now();
+    String updated = (updatedDateTime.year == now.year &&
+        updatedDateTime.month == now.month &&
+        updatedDateTime.day == now.day)
+        ? DateFormat('HH:mm').format(updatedDateTime)
+        : DateFormat('dd MMMM').format(updatedDateTime);
+
+    // build the article item using the sorted data
+    return InkWell(
+        onTap: () async {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) =>
+                  ArticlePage(article: _articles[index]),
+            ),
+          );
+        },
+        child: Padding(
+        padding: const EdgeInsets.all(8.0),
+    child: Row(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+    Container(
+    width: 50,
+    child: Column(
+    children: [
+    Text(
+    updated,
+    style: TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+    ),
+    textAlign: TextAlign.center,
+    ),
+    SizedBox(height: 5),
+    Container(
+    height: 40,
+    child: VerticalDivider(
+    thickness: 2,
+    color: Colors.grey,
+    ),
+    ),
+    ],
+    ),
+    ),
+    SizedBox(width: 10),
+    Expanded(
+    child: Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+            _articles[index]['title'],
+            style: GoogleFonts.encodeSansCondensed(
+                textStyle: TextStyle(
+                    fontWeight: FontWeight.w800,
+                    color: Colors.black,
+                    fontSize: 20,
+                    letterSpacing: 0.5))
+        ),
+        SizedBox(height: 5),
+        Text(
+          _articles[index]['nytdsection'],
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.black45,
+              fontSize: 13),
+        ),
+        SizedBox(height: 10),
+        Divider(
+          color: Colors.grey,
+          thickness: 0.5,
+        ),
+      ],
+    ),
+    ),
+    ],
+    ),
+        ),
+    );
+                },
+            )
+                : Center(child: CircularProgressIndicator()),
+          bottomNavigationBar: MyBottomHomeNavigationBar(currentIndex: 1),
+        ),
     );
   }
 }
+
 class ArticlePage extends StatelessWidget {
   final Map<String, dynamic> article;
 
@@ -138,65 +161,120 @@ class ArticlePage extends StatelessWidget {
   Widget build(BuildContext context) {
     String? imageUrl;
     if (article['media'] != null && article['media'].isNotEmpty) {
-      imageUrl = article['media'][0]['media-metadata'][0]['url'];
+      imageUrl = article['media'][0]['media-metadata'][2]['url'];
     }
     return Scaffold(
       appBar: MyAppBarFeed(),
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 16),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Text(
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+              SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+                article['section'] ?? '',
+                style: GoogleFonts.encodeSansCondensed(
+                    textStyle: TextStyle(
+                        fontWeight: FontWeight.w800,
+                        color: titleColor,
+                        fontSize: 20,
+                        letterSpacing: 0.6))
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
                 article['title'] ?? '',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
+                style: GoogleFonts.encodeSansCondensed(
+                    textStyle: TextStyle(
+                        fontWeight: FontWeight.w800,
+                        color: Colors.black,
+                        fontSize: 20,
+                        letterSpacing: 0.5))
+            ),
+          ),
+          SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              article['updated_date'] ?? '',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey[600],
               ),
             ),
-            SizedBox(height: 16),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Text(
-                article['updated_date'] ?? '',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.grey[600],
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Text(
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
                 article['abstract'] ?? '',
-                style: TextStyle(
-                  fontSize: 18,
-                ),
+                style: GoogleFonts.tinos(
+                    textStyle: TextStyle(
+                        color: Colors.black, fontSize: 18, letterSpacing: 0.6))
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              article['body'] ?? '',
+              style: TextStyle(
+                fontSize: 18,
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                article['body'] ?? '',
-                style: TextStyle(
-                  fontSize: 18,
-                ),
-              ),
+          ),
+          if (imageUrl != null)
+      Image.network(
+      imageUrl,
+      height: 200,
+      width: double.infinity,
+      fit: BoxFit.cover,
+    ),
+    TextButton(
+    onPressed: () async {
+    String articleUrl = article['url'];
+    await FlutterWebBrowser.openWebPage(
+    url: articleUrl,
+    customTabsOptions: CustomTabsOptions(
+    colorScheme: CustomTabsColorScheme.dark,
+    toolbarColor: Theme.of(context).primaryColor,
+    secondaryToolbarColor: Theme.of(context).accentColor,
+    navigationBarColor: Theme.of(context).backgroundColor,
+    addDefaultShareMenuItem: true,
+    instantAppsEnabled: true,
+    showTitle: true,
+      urlBarHidingEnabled: true,
+    ),
+      safariVCOptions: SafariViewControllerOptions(
+        barCollapsingEnabled: true,
+        preferredBarTintColor: Theme.of(context).primaryColor,
+        preferredControlTintColor: Theme.of(context).accentColor,
+        dismissButtonStyle: SafariViewControllerDismissButtonStyle.close,
+        modalPresentationCapturesStatusBarAppearance: true,
+      ),
+    );
+    },
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Align(
+          alignment: Alignment.centerRight,
+          child: Text(
+            'Read the full article on New York Times',
+            style: TextStyle(
+              color: Colors.blue,
+              decoration: TextDecoration.underline,
             ),
-              if (imageUrl != null)
-              Image.network(
-                imageUrl,
-                height: 200,
-                width: double.infinity,
-                fit: BoxFit.cover,
-              ),
-          ],
+          ),
         ),
+      ),
+
+    ),
+              ],
+          ),
       ),
       bottomNavigationBar: BottomBarObject(),
     );
   }
 }
+
+
