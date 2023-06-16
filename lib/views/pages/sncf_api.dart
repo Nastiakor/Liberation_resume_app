70dc255d3c970a31074cfb2a6c93987d589d7bda
Nastiakor <a.korotkova@yahoo.fr>
2023-06-16 17:35:08 +0200

    Affichage OK

--- lib/views/pages/sncf_api.dart
+++ lib/views/pages/sncf_api.dart
@@ -30,67 +30,69 @@ class _SncfAPIState extends State<SncfAPI> {
             Container(
               child: Image.asset('img/entetesncf.png'),
             ),
-            FutureBuilder<List<Train>>(
-                future: futureTrains,
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.done &&
-                      snapshot.hasData) {
-                    print('snap ${snapshot}');
-                    return ListView.builder(
-                        itemCount: snapshot.data!.length,
-                        itemBuilder: (context, index) {
-                          var train = snapshot.data![index];
-                          return Column(
-                            children: [
-                              SizedBox(height: 200),
-                              Text(
-                                'Gare de départ: ${train.departureStation}',
-                                style: TextStyle(color: Colors.white),
-                              ),
-                              Text(
-                                'Jour de départ: ${train.departureDay}',
-                                style: TextStyle(color: Colors.white),
-                              ),
-                              // Ajout du jour de départ
-                              Text(
-                                'Heure de départ: ${train.departureTime}',
-                                style: TextStyle(color: Colors.white),
-                              ),
-                              // Heure de départ en format HH:mm:ss
-                              Text(
-                                'Gare d\'arrivée: ${train.arrivalStation}',
-                                style: TextStyle(color: Colors.white),
-                              ),
-                              Text(
-                                'Jour d\'arrivée: ${train.arrivalDay}',
-                                style: TextStyle(color: Colors.white),
-                              ),
-                              // Ajout du jour d'arrivée
-                              Text(
-                                'Heure d\'arrivée: ${train.arrivalTime}',
-                                style: TextStyle(color: Colors.white),
-                              ),
-                              // Heure d'arrivée en format HH:mm:ss
-                              Text(
-                                'Durée du trajet: ${train.duration}',
-                                style: TextStyle(color: Colors.white),
-                              ),
-                              Text(
-                                'Émissions de CO2: ${train.co2Emission.toStringAsFixed(2)} kg',
-                                style: TextStyle(color: Colors.white),
-                              ),
-                            ],
-                          );
-                        });
-                  } else if (snapshot.connectionState == ConnectionState.done &&
-                      snapshot.hasError) {
-                    return Text('ERREUR : ${snapshot.error}');
-                    // return const CircularProgressIndicator();
-                  } else {
-                    // Pendant ce temps, le hamster tourne dans la roue.
-                    return CircularProgressIndicator();
-                  }
-                }),
+            Expanded(
+              child: FutureBuilder<List<Train>>(
+                  future: futureTrains,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done &&
+                        snapshot.hasData) {
+                      print('snap ${snapshot}');
+                      return ListView.builder(
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (context, index) {
+                            var train = snapshot.data![index];
+                            return Column(
+                              children: [
+                                Text(
+                                  'Gare de départ: ${train.departureStation}',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                Text(
+                                  'Jour de départ: ${train.departureDay}',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                // Ajout du jour de départ
+                                Text(
+                                  'Heure de départ: ${train.departureTime}',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                // Heure de départ en format HH:mm:ss
+                                Text(
+                                  'Gare d\'arrivée: ${train.arrivalStation}',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                Text(
+                                  'Jour d\'arrivée: ${train.arrivalDay}',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                // Ajout du jour d'arrivée
+                                Text(
+                                  'Heure d\'arrivée: ${train.arrivalTime}',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                // Heure d'arrivée en format HH:mm:ss
+                                Text(
+                                  'Durée du trajet: ${train.duration}',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                Text(
+                                  'Émissions de CO2: ${train.co2Emission.toStringAsFixed(2)} kg',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ],
+                            );
+                          });
+                    } else if (snapshot.connectionState ==
+                            ConnectionState.done &&
+                        snapshot.hasError) {
+                      return Text('ERREUR : ${snapshot.error}');
+                      // return const CircularProgressIndicator();
+                    } else {
+                      // Pendant ce temps, le hamster tourne dans la roue.
+                      return CircularProgressIndicator();
+                    }
+                  }),
+            ),
           ]),
         ),
         bottomNavigationBar: MyBottomHomeNavigationBar(currentIndex: 1),
