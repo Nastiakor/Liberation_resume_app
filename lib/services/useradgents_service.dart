753c97df3b32e4d56af4e7377a16eea970076776
Nastiakor <a.korotkova@yahoo.fr>
2023-06-21 17:39:58 +0200

    API Useradgents MITM

---
+++ lib/services/useradgents_service.dart
@@ -0,0 +1,15 @@
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:convert/convert.dart';
+import 'package:cv_flutter_libe/models/useradgents.dart';
+
+class UseradgentsService {
+  static Future<http.Response> fetchUseradgentsFeed() async {
+    print(http.get(
+        Uri.parse('https://www.useradgents.com/api/wp-json/v1/fridaywatch')));
+    return http.get(
+        Uri.parse('https://www.useradgents.com/api/wp-json/v1/fridaywatch'));
+  }
+}
+
