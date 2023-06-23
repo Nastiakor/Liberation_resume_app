9ffbbd744103219b1c5a112a05b24a5eee1557c1
Nastiakor <a.korotkova@yahoo.fr>
2023-06-23 20:30:26 +0200

    5 articles récupérés

--- lib/views/pages/useradgents_feed.dart
+++ lib/views/pages/useradgents_feed.dart
@@ -3,6 +3,8 @@ import 'package:cv_flutter_libe/views/pages/useradgents_feed.dart';
 import 'package:cv_flutter_libe/services/useradgents_service.dart';
 import 'package:cv_flutter_libe/models/useradgents.dart';
 import 'dart:convert';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:cv_flutter_libe/views/pages/useradgetsFullArticle.dart';
 
 class UseradgentsPage extends StatefulWidget {
   const UseradgentsPage({Key? key}) : super(key: key);
@@ -15,20 +17,30 @@ class _UseradgentsPageState extends State<UseradgentsPage> {
   List<Useradgents> _UseradgentsContent = [];
 
   @override
+  void initState() {
+    super.initState();
+    _fetchUseradgentsJSON();
+  }
+
   _fetchUseradgentsJSON() async {
     var response = await UseradgentsService.fetchUseradgentsFeed();
     print(response);
 
     if (response.statusCode == 200) {
       // Si la requête a réussi
-      // Convertir le corps de la réponse en une liste
+      // Convertir le corps de la réponse en une map
       List<dynamic> jsonResponse = jsonDecode(response.body);
+      print(jsonResponse);
 
-      // Convertir chaque élément de la liste en un objet Useradgents
+      // Convertir la map en une liste de maps
+      List<Useradgents> useradgentsList =
+          jsonResponse.map((data) => Useradgents.fromJson(data)).toList();
       setState(() {
-        _UseradgentsContent =
-            jsonResponse.map((data) => Useradgents.fromJson(data)).toList();
+        _UseradgentsContent = useradgentsList;
       });
+      print('first title${useradgentsList[0].title}');
+
+      // Convertir chaque map en un objet Useradgents
     } else {
       // Gérer l'erreur
       print('Erreur lors de la récupération des données');
@@ -39,7 +51,24 @@ class _UseradgentsPageState extends State<UseradgentsPage> {
     return MaterialApp(
       title: 'Useradgents feed',
       home: Scaffold(
-        body: Container(),
+        body: ListView.builder(
+            itemCount: _UseradgentsContent.length,
+            itemBuilder: (context, i) {
+              return InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                      MaterialPageRoute(
+                      builder: (context) => UseradgentsFullArticle(content: _UseradgentsContent[i].content ?? ''), // Passe le contenu ici
+                  ),);
+                },
+                child: ListTile(
+                  title: Text(_UseradgentsContent[i].title ?? ''),
+                  subtitle:
+                      Html(data: _UseradgentsContent[i].shortDescription ?? ''),
+                ),
+              );
+            }),
       ),
     );
   }
