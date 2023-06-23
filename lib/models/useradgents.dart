084a3c0fa5278c941b3344de0421916dc210fe7c
Johan <anquetil.johan@laposte.net>
2023-06-24 01:34:13 +0200

    useradgents article avant animation

--- lib/models/useradgents.dart
+++ lib/models/useradgents.dart
@@ -1,3 +1,43 @@
+class Author {
+  final String? name;
+  final String? lastname;
+  final String? post;
+  final String? photo;
+
+  Author({
+    this.name,
+    this.lastname,
+    this.post,
+    this.photo,
+  });
+
+  factory Author.fromJson(Map<String, dynamic> json) {
+    return Author(
+      name: json['name']?? '',
+      lastname: json['lastname']?? '',
+      post: json['post']?? '',
+      photo: json['photo']?? '',
+    );
+  }
+}
+
+class FeaturedImage {
+  final String? type;
+  final String? src;
+
+  FeaturedImage({
+    this.type,
+    this.src,
+  });
+
+  factory FeaturedImage.fromJson(Map<String, dynamic> json) {
+    return FeaturedImage(
+      type: json['type']?? '',
+      src: json['src']?? '',
+    );
+  }
+}
+
 class Useradgents {
   final String? lastname;
   final String? name;
@@ -8,8 +48,11 @@ class Useradgents {
   final String? shortDescription;
   final List<dynamic>? themes;
   final String? title;
+  final List<Author>? authors;
+  final List<FeaturedImage>? featuredImages;
+  final String? readingTime; // Ajout du champ readingTime
 
-  const Useradgents ({
+  const Useradgents({
     this.lastname,
     this.name,
     this.photo,
@@ -18,19 +61,35 @@ class Useradgents {
     this.date,
     this.shortDescription,
     this.themes,
-    this.title
-});
+    this.title,
+    this.authors,
+    this.featuredImages,
+    this.readingTime,
+  });
+
   factory Useradgents.fromJson(Map<String, dynamic> json) {
+    List<Author> authors = List<Map<String, dynamic>>.from(json['authors'])
+        .map((authorJson) => Author.fromJson(authorJson))
+        .toList();
+
+    List<FeaturedImage> featuredImages = List<Map<String, dynamic>>.from(json['featuredMedias'])
+        .map((imageJson) => FeaturedImage.fromJson(imageJson))
+        .toList();
+
     return Useradgents(
-      lastname: json['lastname']?? '',
-      name: json['name']?? '',
-      photo: json['photo']?? '',
-      post: json['post']?? '',
-      content: json['content']?? '',
-      date: json['date']?? '',
-      shortDescription: json['shortDescription']?? '',
-      themes: json['themes']?? '',
-      title: json['title']?? ''
+      lastname: json['lastname'] ?? '',
+      name: json['name'] ?? '',
+      photo: json['photo'] ?? '',
+      post: json['post'] ?? '',
+      content: json['content'] ?? '',
+      date: json['date'] ?? '',
+      shortDescription: json['shortDescription'] ?? '',
+      themes: json['themes'] ?? '',
+      title: json['title'] ?? '',
+      authors: authors,
+      featuredImages: featuredImages,
+      readingTime: json['readingTime'], // Lecture du champ readingTime
     );
   }
 }
+
--- lib/views/pages/useradgents_feed.dart
+++ lib/views/pages/useradgents_feed.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:cv_flutter_libe/views/pages/useradgents_feed.dart';
+import 'package:cv_flutter_libe/views/pages/useradgetsFullArticle.dart';
 import 'package:cv_flutter_libe/services/useradgents_service.dart';
 import 'package:cv_flutter_libe/models/useradgents.dart';
-import 'dart:convert';
 import 'package:flutter_html/flutter_html.dart';
-import 'package:cv_flutter_libe/views/pages/useradgetsFullArticle.dart';
+import 'dart:convert';
 
 class UseradgentsPage extends StatefulWidget {
   const UseradgentsPage({Key? key}) : super(key: key);
@@ -27,22 +26,16 @@ class _UseradgentsPageState extends State<UseradgentsPage> {
     print(response);
 
     if (response.statusCode == 200) {
-      // Si la requête a réussi
-      // Convertir le corps de la réponse en une map
       List<dynamic> jsonResponse = jsonDecode(response.body);
       print(jsonResponse);
 
-      // Convertir la map en une liste de maps
       List<Useradgents> useradgentsList =
-          jsonResponse.map((data) => Useradgents.fromJson(data)).toList();
+      jsonResponse.map((data) => Useradgents.fromJson(data)).toList();
       setState(() {
         _UseradgentsContent = useradgentsList;
       });
       print('first title${useradgentsList[0].title}');
-
-      // Convertir chaque map en un objet Useradgents
     } else {
-      // Gérer l'erreur
       print('Erreur lors de la récupération des données');
     }
   }
@@ -50,26 +43,111 @@ class _UseradgentsPageState extends State<UseradgentsPage> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Useradgents feed',
+      theme: ThemeData(
+        primaryColor: Colors.black,
+        fontFamily: 'Helvetica',
+      ),
       home: Scaffold(
+        appBar: AppBar(
+          title: Text('Useradgents'),
+          centerTitle: true,
+        ),
         body: ListView.builder(
-            itemCount: _UseradgentsContent.length,
-            itemBuilder: (context, i) {
-              return InkWell(
+          itemCount: _UseradgentsContent.length,
+          itemBuilder: (context, i) {
+            final useradgents = _UseradgentsContent[i];
+
+            return Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: InkWell(
                 onTap: () {
                   Navigator.push(
                     context,
-                      MaterialPageRoute(
-                      builder: (context) => UseradgentsFullArticle(content: _UseradgentsContent[i].content ?? ''), // Passe le contenu ici
-                  ),);
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          UseradgentsFullArticle(useradgents: useradgents),
+                    ),
+                  );
                 },
-                child: ListTile(
-                  title: Text(_UseradgentsContent[i].title ?? ''),
-                  subtitle:
-                      Html(data: _UseradgentsContent[i].shortDescription ?? ''),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 1,
+                        blurRadius: 7,
+                        offset: Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.vertical(
+                          top: Radius.circular(15),
+                        ),
+                        child: useradgents.featuredImages != null && useradgents.featuredImages!.isNotEmpty
+                            ? Image.network(useradgents.featuredImages!.first.src ?? '', height: 200, width: double.infinity, fit: BoxFit.cover)
+                            : Container(color: Colors.grey, height: 200, width: double.infinity),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              useradgents.title ?? '',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                                color: Colors.black,
+                              ),
+                            ),
+                            SizedBox(height: 8),
+                            Html(
+                              data: useradgents.shortDescription ?? '',
+                              style: {
+                                "body": Style(
+                                  fontSize: FontSize(14),
+                                  color: Colors.grey,
+                                ),
+                              },
+                            ),
+                            SizedBox(height: 8),
+                            Row(
+                              children: [
+                                if (useradgents.authors != null && useradgents.authors!.isNotEmpty)
+                                  CircleAvatar(
+                                    backgroundImage: NetworkImage(useradgents.authors!.first.photo ?? ''),
+                                  ),
+                                SizedBox(width: 8),
+                                if (useradgents.authors != null && useradgents.authors!.isNotEmpty)
+                                  Text(
+                                    "${useradgents.authors!.first.name} ${useradgents.authors!.first.lastname}",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 14,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              );
-            }),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
+
+
 }
